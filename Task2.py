0141642fd88aa979039df72a676b627f424eacbb
Ivan Sidorenko <cidor.0712@gmail.com>
2022-11-29 13:52:01 +0500

    Task2

---
+++ Task2.py
@@ -0,0 +1,16 @@
+# Напишите программу, которая принимает на вход число N
+# и выдает набор произведений (набор - это список) чисел от 1 до N.
+# Не используйте функцию math.factorial.
+# Пример:
+# - пусть N = 4, тогда [ 1, 2, 6, 24 ] #(1, 1*2, 1*2*3, 1*2*3*4)
+
+
+n = int(input('Введите число: '))
+
+
+def factorial(x):
+    return 1 if x == 0 else x * factorial(x - 1)
+
+
+listFact = list(factorial(i) for i in range(1, n + 1))
+print(listFact)
